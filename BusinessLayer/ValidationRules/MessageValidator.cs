be6719c494dbddc7eeaf2c08a70f4f0698c5592c
burak551 <70866473+burak551@users.noreply.github.com>
2021-07-01 20:48:29 +0300

    Kucuk Duzeltmeler

---
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı Adresini Boş Geçemezsiniz");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konuyu Boş Geçemezsiniz");
+            RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı Boş Geçemezsiniz");
+            RuleFor(x => x.ReceiverMail).EmailAddress().WithMessage("Lütfen Geçerli Bir Mail Adresi Girişi Yapın");
+            RuleFor(x => x.Subject).MaximumLength(100).WithMessage("Lütfen 100 Karekter Fazla Değer Girişi Yapmayın");
+        }
+    }
+}
